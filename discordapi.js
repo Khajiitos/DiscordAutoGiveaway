8baeedbc12cdabc4b49bbfc3c10adbe8435b9b4d
Khajiitos <khajiit112@gmail.com>
2022-11-26 13:40:09 +0100

    More work on the api handler. still not working lol

--- discordapi.js
+++ discordapi.js
@@ -1,6 +1,7 @@
 const WS = require('ws');
 
-const websocketUrl = 'wss://gateway.discord.gg/?v=9&encoding=json';
+const websocketUrl = 'wss://gateway.discord.gg';
+const webSocketGetParams = '/?v=9&encoding=json';
 
 const defaultIdentifySettings = {
     op: 2,
@@ -34,18 +35,22 @@ class DiscordApi {
     token = null;
     seq = null;
     websocket = null;
+    session_id = null;
     heartbeatTimeout = null;
     heartbeat_interval = null;
     lastHeartbeatAcknowledged = true;
     heartbeatNotAcknowledgedTimeout = null;
+    resume_gateway_url = null;
+    resumeNotAcknowledgedTimeout = null;
+    resumeRetryDelay = 30;
 
     constructor(token) {
         this.token = token;
-        this.initWebsocket();
+        this.initWebsocket(false);
     }
 
-    initWebsocket() {
-        this.websocket = new WS.WebSocket(websocketUrl);
+    initWebsocket(resume = false) {
+        this.websocket = new WS.WebSocket((resume === true ? this.resume_gateway_url : websocketUrl) + webSocketGetParams);
 
         this.websocket.on('open', () => {
             console.log('[Discord API] Opened WebSocket');
@@ -63,18 +68,29 @@ class DiscordApi {
                 case 10: // HELLO
                     this.heartbeat_interval = json.heartbeat_interval;
                     this.initHeartbeatLoop();
-                    this.sendIdentify();
+                    if (resume) {
+                        this.resume();
+                    } else {
+                        this.sendIdentify();
+                    }
                     break;
                 case 11: // HEARTBEAT_ACK
                     clearTimeout(this.heartbeatNotAcknowledgedTimeout);
                     this.lastHeartbeatAcknowledged = true;
                     break;
-                case 7:
+                case 9: // INVALID_SESSION
                     console.log('[Discord API] Invalid Session');
+                    break;
+                case 7: // RECONNECT
+                    console.log('[Discord API] Discord wants us to reconnect.');
+                    this.close();
+                    this.initWebsocket();
                     break;
                 case 0:
                     if (json.t === 'READY') {
                         console.log('[Discord API] Ready, connected as %s#%s', json.d.user.username, json.d.user.discriminator);
+                        this.session_id = json.d.session_id;
+                        this.resume_gateway_url = json.d.resume_gateway_url;
                     }
                     break;
             }
@@ -91,14 +107,15 @@ class DiscordApi {
     }
 
     sendHeartbeat() {
+        this.lastHeartbeatAcknowledged = false;
+        this.heartbeatNotAcknowledgedTimeout = setTimeout(() => {
+            
+            console.log("[Discord API] Last heartbeat hasn't been acknowledged in 15 seconds. Connection problems?");
+        }, 15000);
         this.websocket.send(JSON.stringify({
             op: 1,
             d: this.seq
         }));
-        this.lastHeartbeatAcknowledged = false;
-        this.heartbeatNotAcknowledgedTimeout = setTimeout(() => {
-            console.log("[Discord API] Last heartbeat hasn't been acknowledged in 15 seconds. Connection problems?");
-        }, 15000);
     }
 
     sendIdentify() {
@@ -110,7 +127,25 @@ class DiscordApi {
     close() {
         clearTimeout(this.heartbeatTimeout);
         clearTimeout(this.heartbeatNotAcknowledgedTimeout);
+        this.websocket.close();
+    }
+
+    resume() {
+        this.websocket.send(JSON.stringify({
+            op: 6,
+            d: {
+                token: this.token,
+                session_id: this.session_id,
+                seq: this.seq
+            }
+        }));
+
+        this.resumeNotAcknowledgedTimeout = setTimeout(() => {
+            console.log("[Discord API] Tried to resume, but we didn't get a response. Connection error? Trying again in %d seconds.", this.resumeRetryDelay);
+            this.resume();
+            this.resumeRetryDelay += 30;
+        }, this.resumeRetryDelay);
     }
 }
 
-module.exports = {DiscordApi};
+module.exports = { DiscordApi };
